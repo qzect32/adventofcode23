d9922a2bed181762b7ce1e76bbe76e6686fb71bb
Payne Patzke <paynewp@gmail.com>
2023-12-18 17:53:35 -0500

    asas

--- Adv1.py
+++ Adv1.py
@@ -1,75 +1,43 @@
 #PWP/Dec 18, 2023/Advent of Code 1
 import re
 
-file_path = 'C:\\Users\\pwpat\\OneDrive\\Documents\\GitHub\\adventofcode23\\adv1_text.txt'
-total_sum = 0
+numbers = ["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-def extract_and_sum_calibration_values(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            total_count = 0
-            for line in lines:
-                line = line.strip()  # Remove leading/trailing whitespace
-                first_digit = None
-                last_digit = None
+def convert(value):
+  if value is not None:
+    if value.isdigit():
+      return int(value)
+    if value in numbers:
+      return int(numbers.index(value))
 
-                for char in line:
-                    if char.isdigit():
-                        if first_digit is None:
-                            first_digit = int(char)
-                        last_digit = int(char)
+with open('adv1_text.txt') as f:
+  
+  lines = f.readlines()
 
-                if first_digit is not None and last_digit is not None:
-                    two_digit_number = first_digit * 10 + last_digit
-                    total_count += two_digit_number
+  count = 0
 
-            return total_count
-    except FileNotFoundError:
-        return None
+  for line in lines:
+    dictionary = {}
 
-def calculate_calibration_value(file_path):
-    total_sum = 0
+    # Get digit indexes
+    for index, c in enumerate(line): 
+      if c.isdigit():
+        dictionary[index] = convert(c)
+
+    # Get numbers in words indexes
+    for number in numbers:
+      matches = re.finditer(number, line)
+
+      for match in matches:
+        dictionary[match.span()[0]] = convert(number)
+
+    # Get first and last dictionary items
+    keys = sorted(dictionary.keys())
+    count += (dictionary[keys[0]] * 10) + dictionary[keys[-1]]
+
+  print(count)
     
-    with open(file_path, 'r') as file:
-        for line in file:
-            # Initialize variables to store the first and last digits
-            first_digit = None
-            last_digit = None
-            
-            # Iterate through characters in the line
-            for char in line:
-                # Check if the character is a digit (0-9) or a spelled-out digit word
-                if char.isdigit():
-                    digit = int(char)
-                    if first_digit is None:
-                        first_digit = digit
-                    last_digit = digit
-                elif char.isalpha():
-                    # Define a dictionary to map spelled-out digit words to their numerical values
-                    word_to_digit = {
-                        'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
-                        'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
-                    }
-                    
-                    # Check if the character is part of a spelled-out digit word
-                    word_buffer = char
-                    while word_buffer in word_to_digit:
-                        digit = word_to_digit[word_buffer]
-                        if first_digit is None:
-                            first_digit = digit
-                        last_digit = digit
-                        # Continue reading characters to complete the word
-                        word_buffer += line[len(word_buffer)]
-            
-            # If both first_digit and last_digit were found, calculate and add to the total_sum
-            if first_digit is not None and last_digit is not None:
-                combined_value = first_digit * 10 + last_digit
-                total_sum += combined_value
-    
-    return total_sum
-
 # Call the function to calculate the sum of all calibration values
 #result = extract_and_sum_calibration_values(file_path)
-total_count = calculate_calibration_value(file_path)
-print( "the sum of the calibration value:  ", total_count)
+#total_count = extract_and_sum_calibration_values(file_path)
+#print( "the sum of the calibration value:  ", total_count)
